3fdc853d4d793288abc47dce2730052ab8ffb4c0
hihiroo <alsrud606@naver.com>
2021-11-02 17:37:26 +0900

    프로젝트3 뼈대 코드 및 설명 업로드

---
+++ project#3/miller_rabin.skeleton.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020, 2021. Heekuck Oh, all rights reserved
+ * 이 프로그램은 한양대학교 ERICA 소프트웨어학부 재학생을 위한 교육용으로 제작되었습니다.
+ */
+#include "miller_rabin.h"
+
+/*
+ * Miller-Rabin Primality Testing against small sets of bases
+ *
+ * if n < 2^64,
+ * it is enough to test a = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, and 37.
+ *
+ * if n < 3,317,044,064,679,887,385,961,981,
+ * it is enough to test a = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, and 41.
+ */
+const uint64_t a[ALEN] = {2,3,5,7,11,13,17,19,23,29,31,37};
+
+/*
+ * miller_rabin() - Miller-Rabin Primality Test (deterministic version)
+ *
+ * n > 3, an odd integer to be tested for primality
+ * It returns 1 if n is prime, 0 otherwise.
+ */
+int miller_rabin(uint64_t n)
+{
+// 여기를 완성하세요
+}
